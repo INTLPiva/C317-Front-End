8c92461439828e788bb63701f3bf9e4baf89cacd
Lucas Piva <lucas.piva@gec.inatel.br>
2023-04-11 11:22:54 -0300

    fix: route file name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import { AppRoutes } from './routes/indes';
+import { AppRoutes } from './routes';
 import { GlobalStyle } from './styles/global';
 
 export const App = () => {
