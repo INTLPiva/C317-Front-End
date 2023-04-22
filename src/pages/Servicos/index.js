1a09714dcf0cdda8364ed64f00e7e2e32f27bbec
Lucas Piva <lucas.piva@gec.inatel.br>
2023-04-22 19:52:13 -0300

    feat: services page

--- src/pages/Servicos/index.js
+++ src/pages/Servicos/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
+import data from './database.json';
+import { Container } from './styles';
+import { Card } from '../../components/Card';
 import { Header } from '../../components/Header';
 
 export const Servicos = () => {
   return (
-    <>
+    <Container>
       <Header />
-      <h1>servicos</h1>
-    </>
+      <Card>
+        {data.map((element) => (
+          <div className="dataContainer" key={element.id}>
+            <div>
+              <p>{element.nome}</p>
+              <p>E-mail: {element.email}</p>
+              <p>Contato: {element.contato}</p>
+              <p>Endereço: {element.endereco}</p>
+            </div>
+            <h2>{element.servico}</h2>
+          </div>
+        ))}
+      </Card>
+    </Container>
   );
 };
---
+++ src/pages/Servicos/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  p {
+    margin-bottom: 8px;
+  }
+
+  div {
+    div {
+      background-color: #ddd7dd;
+      padding: 16px;
+    }
+
+    .dataContainer {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      margin-bottom: 16px;
+    }
+  }
+`;
